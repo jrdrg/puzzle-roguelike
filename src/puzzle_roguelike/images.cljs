54b1787f842d744a1a0491c727a6923cd99205b7
John Rodriguez <jrodriguez.0987+github@gmail.com>
2016-01-31 20:54:26 -0500

    Switched to new tileset

--- src/puzzle_roguelike/images.cljs
+++ src/puzzle_roguelike/images.cljs
@@ -1,10 +1,10 @@
 (ns puzzle-roguelike.images )
 
 
-(def sprite-image-size [16 16])
+(def sprite-image-size [16 24])
 
 (defn- sprite-coords [x y]
   (let [[sprite-x sprite-y] sprite-image-size]
     [(- (* x sprite-x)) (- (* y sprite-y))]))
 
-(def player {:key player :sprite (sprite-coords 0 4)})
+(def player {:key player :sprite (sprite-coords 0 0)})
